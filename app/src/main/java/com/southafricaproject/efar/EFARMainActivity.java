e3f38b33e92b11b68dc50270f696209dcfddf75d
Tony Thumpasery <tthump14@gmail.com>
2017-04-14 17:35:36 -0400

    one more time lol

--- app/src/main/java/com/southafricaproject/efar/EFARMainActivity.java
+++ app/src/main/java/com/southafricaproject/efar/EFARMainActivity.java
@@ -3,7 +3,11 @@ package com.southafricaproject.efar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class EFARMainActivity extends AppCompatActivity {
 
@@ -11,6 +15,26 @@ public class EFARMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_efarmain);
+
+        ArrayList<String> patientArray = new ArrayList<String>();
+        patientArray.add("item1");
+        patientArray.add("item2");
+        patientArray.add("fugly bob");
+        patientArray.add("Nelson");
+
+        //instantiate custom adapter
+//        MyCustomAdapter adapter = new MyCustomAdapter(patientArray, this);
+
+        //handle listview and assign adapter
+//        ListView lView = (ListView)findViewById(R.id.patient_list);
+//        lView.setAdapter(adapter);
+
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, patientArray);
+
+        ListView listView = (ListView) findViewById(R.id.patient_list);
+        listView.setAdapter(adapter);
 
         //button to get back to patient screen
         Button logoutButton = (Button) findViewById(R.id.logout_button);
