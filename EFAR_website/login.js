e1cfb85267479b0cbce172d597a3c5a310a5ff2d
Anli Zhang <anlizhang@Anlis-MacBook-Air.local>
2017-10-21 14:35:06 -0400

    stufff

--- EFAR_website/login.js
+++ EFAR_website/login.js
@@ -30,37 +30,4 @@ function check_username(){
           window.location = 'message.html';
         } 
       });
-
-  	/*if(username_entered == ""){
-  		alertify.error("No username! Try Again...");
-  	}else{
-  		database.ref("/users").once('value', function(snapshot) {
-			if(snapshot.hasChild(username_entered)) {
-				check_password(password_entered, username_entered);
-			}else{
-				alertify.error("Wrong username! Try Again...");
-			}
-	    });
-  	}*/
-}
-
-function check_password(password_entered, username_entered){
-	if(password_entered == ""){
-  		alertify.error("No password! Try Again...");
-  	}else{
-  		database.ref("/users/" + username_entered + '/name').once('value', function(snapshot) {
-			if(snapshot.val() == password_entered) {
-		    	alertify.success("Welcome " + username_entered + "!");
-		    	firebase.auth().signInAnonymously().catch(function(error) {
-				  // Handle Errors here.
-				  var errorCode = error.code;
-				  var errorMessage = error.message;
-				  // ...
-				});
-				window.location = 'message.html';
-		  	}else{
-		  		alertify.error("Wrong password! Try Again...");
-		  	}
-		});
-  	}	
 }
