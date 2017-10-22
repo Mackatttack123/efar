93762835123dcc0aabf672ae178cc30516b454e8
Mack FitzPatrick <fitzpatrick1@college.harvard.edu>
2017-10-21 23:09:40 -0400

    buttons for dispatchers

--- EFAR_website/contact-efar.js
+++ EFAR_website/contact-efar.js
@@ -10,6 +10,9 @@ function setup() {
 var efar_name;
 var efar_id;
 var efar_phone;
+var home_button;
+var start_chat_button;
+var button_created = false;
 
 function draw(){
 	var state;
@@ -28,10 +31,29 @@ function draw(){
 		  console.log("The read failed: " + errorObject.code);
 		});
 		waiting_p.html("<h1><center>EFAR: " + efar_name + "<p>ID #: " + efar_id + "</p><p>Phone: " + efar_phone + "</p></center></h1>");
+		start_chat_button = createButton("Start Chat").class("btn btn-primary");
+		start_chat_button.mousePressed(startchat);
+		start_chat_button.position(windowWidth/2-40, windowHeight/2);
+		home_button.hide();
 	}else if(state == "0"){
 		waiting_p.html("<h1><center>Waiting for EFAR...</center></h1>");
+		home_button.hide();
 	}else{
-		waiting_p.html("<h1><center>The EFAR has ended the emergency.</center></h1>");
+		if(!button_created){
+			button_created = true;
+			waiting_p.html("<h1><center>The EFAR has ended the emergency.<p style='padding-top: 50px'></center></h1>");
+			home_button = createButton("Return Home").class("btn btn-primary");
+			home_button.mousePressed(gohome);
+		}
+		home_button.position(windowWidth/2-40, windowHeight/2);
 	}
 	
+}
+
+function gohome(){
+	window.location = 'home.html';
+}
+
+function startchat(){
+	window.location = 'message.html';
 }
