9f24bc9d736f0c8042e2980472eb9680b4765b31
Mack FitzPatrick <fitzpatrick1@college.harvard.edu>
2017-12-30 00:25:53 -0600

    can now sort efars based on postion to emergency and only send notification to the clostest efar

--- functions/index.js
+++ functions/index.js
@@ -3,68 +3,56 @@ function Efar (distance_away, token, message) {
     this.token = token;
     this.message = message;
 
-    function getToken() {
-        return token;
-    };
-    function getMessage() {
-        return message;
-    };
+    this.getToken = function() {
+        return this.token;
+    }
+    this.getMessage = function() {
+        return this.message;
+    }
 }
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-efarArray = [];
-
 exports.sendPushNotificationAdded = functions.database.ref('/emergencies/{id}').onCreate(event => {
-	return admin.database().ref('tokens').once("value").then(allToken => {
-		if (allToken.val()){
-			const token = Object.keys(allToken.val());
-			tokenSplit = String(token).split(",");
-			buildEFARs(tokenSplit, event.data.child('latitude').val(), event.data.child('longitude').val());
-			sortEfars();
-			/*realtoken = tokenSplit[0];
-			admin.database().ref('tokens/' + realtoken).once('value').then(function(snapshot) {
-				var distance_to_efar = distance(parseFloat(snapshot.child('latitude').val()), parseFloat(snapshot.child('longitude').val()), event.data.child('latitude').val(), event.data.child('longitude').val());
-				efarArray.push(new Efar(distance_to_efar, realtoken));
-				var payload = {
-					notification: {
-						title: "NEW EMERGANCY!",
-						//body: "Info given: \"" + event.data.child('other_info').val() + "\"",
-						body: "Distace from you: " + distance_to_efar + "km numEFAR: " + efarArray.length,
-						//badge: '1',
-						sound: 'default',
-					}
-				};
-				return admin.messaging().sendToDevice(token, payload).then(response => {
-					
-				});
-			});*/
-			return admin.messaging().sendToDevice(efarArray[0].getToken, efarArray[0].getMessage).then(response => {
-					
-			});
+	return admin.database().ref('/tokens').on('value', function(snapshot) {
+	    var efarArray = snapshotToArray(snapshot, event.data.child('latitude').val(), event.data.child('longitude').val());
+	    efarArray.sort(function(a, b) {
+		    return a.distance - b.distance;
+		});
+	    var payload = {
+			notification: {
+				title: "NEW EMERGANCY!",
+				body: "Info given:" + event.data.child('other_info').val(),
+				//badge: '1',
+				sound: 'default',
+			}
 		};
+	    return admin.messaging().sendToDevice(efarArray[0].token, payload).then(response => {
+					
+		});
 	});
 });
 
-function buildEFARs(tokenlist, emergancy_lat, emergancy_long){
-	for (var i = tokenlist.length - 1; i >= 0; i--) {
-		admin.database().ref('tokens/' + tokenlist[i]).once('value').then(function(snapshot) {
-			var distance_to_efar = distance(parseFloat(snapshot.child('latitude').val()), parseFloat(snapshot.child('longitude').val()), emergancy_lat, emergancy_long);
-			var payload = {
-				notification: {
-					title: "NEW EMERGANCY!",
-					//body: "Info given: \"" + event.data.child('other_info').val() + "\"",
-					body: "Distace from you: " + distance_to_efar + "km",
-					//badge: '1',
-					sound: 'default',
-				}
-			};
-			efarArray.push(new Efar(distance_to_efar, tokenlist[i], payload));
-		});
-	}
-}
+
+function snapshotToArray(snapshot, incoming_latitude, incoming_longitude) {
+    var returnArr = [];
+
+    snapshot.forEach(function(childSnapshot) {
+    	var distance_to_efar = distance(childSnapshot.child('latitude').val(), childSnapshot.child('longitude').val(), incoming_latitude, incoming_longitude);
+        var item = {
+        	latitude: childSnapshot.child('latitude').val(), 
+        	longitude: childSnapshot.child('longitude').val(),
+        	token: childSnapshot.key,
+        	distance: distance_to_efar
+        };
+
+        returnArr.push(item);
+    });
+
+    return returnArr;
+};
 
 exports.sendPushNotificationCanceled = functions.database.ref('/canceled/{id}').onCreate(event => {
 	const payload = {
@@ -116,94 +104,3 @@ function distance(lat1, lon1, lat2, lon2) {
 	dist = dist * 1.609344;
 	return dist;
 }
-
-// TODO: For some odd reason the distance setter or caulator is returning null? or not working
-/* POSSIBLE WAY TO KEEP TRACK OF SORTED LISTS?
-	sorted
-	|
-	|
-	 -> KEY : JGYUKAHISLJDHJV
-	 	|
-	 	|
-	 	 -> mack
-	 	|
-	 	|
-	 	 -> jim
-	 	|
-	 	|
-	 	 -> etc....
-	|
-	|
-	 -> KEY : GUILKJFGKHLJHLJ
-	|
-	|
-	 -> KEY : LJKHGFJKL;JHJLKH
-*/
-
-function sortEfars() {
-	efarArray.sort(function(a, b) { 
-	    return a.distance_away - b.distance_away;
-	});
-}
-
-/*exports.sendPushNotification = functions.database.ref('/emergencies/{id}').onCreate(event => {
-	const payload = {
-		notification: {
-			title: "NEW EMERGANCY:",
-			body: "Info given: \"" + event.data.child('other_info').val() + "\"",
-			badge: '1',
-			sound: 'default',
-		}
-	};
-
-	efarArray = [];
-	return admin.database().ref('/users').on('value', function(snapshot){
-	    snapshot.forEach(function(child){
-	        var name = child.child("name").val();
-	        var lat = 0.0;
-	        var long = 0.0;
-	        var token = "token"
-	        if(child.child("latitude").exists()){
-	        	lat = child.child("latitude").val();
-	        }
-	        if(child.child("longitude").exists()){
-		        long = child.child("longitude").val();
-		    }
-		    if(child.child("token").exists()){
-		        token = child.child("token").val();
-		    }
-	        efarArray.push(new Efar(name, lat, long, token));
-	    });
-
-
-	    sortEfars(event.data.child('latitude').val(), event.data.child('longitude').val());
-
-
-		for (var i = 0; i < efarArray.length; i++) {
-				var new_key = admin.database().ref('/sorted/' + efarArray[i].name);
-		        new_key.child("distance").set(efarArray[i].distance);
-		}
-
-		return admin.database().ref('tokens').once("value").then(allToken => {
-			if (allToken.val()){
-				const token = Object.keys(allToken.val());
-				return admin.messaging().sendToDevice(token, payload).then(response => {
-
-				});
-			};
-		});
-	});
-	return admin.database().ref('tokens').once("value").then(allToken => {
-		if (allToken.val()){
-			const token = Object.keys(allToken.val());
-			return admin.messaging().sendToDevice(token, payload).then(response => {
-
-			});
-		};
-	});
-		
-});*/
-	
-
-
-
